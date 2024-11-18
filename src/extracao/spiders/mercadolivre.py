382d2d959e279077a3c81973413613ed095766a3
Lucas de Jesus <lucas.scidata@gmail.com>
2024-11-18 14:32:26 -0300

    adicionando no webscraping  nome, preco_antigo, preco_novo, rating, qtd_reviews.

--- src/extracao/spiders/mercadolivre.py
+++ src/extracao/spiders/mercadolivre.py
@@ -13,7 +13,14 @@ class MercadolivreSpider(scrapy.Spider):
         produtos = response.css('div.ui-search-result__wrapper')
 
         for produto in produtos:
+
+            precos = produto.css('span.andes-money-amount__fraction::text').getall()
             
             yield{
-                'brand': produto.css('span.poly-component__brand::text').get()
+                'brand': produto.css('span.poly-component__brand::text').get(),
+                'nome': produto.css('h2.poly-box.poly-component__title a::text').get(),
+                'preco_antigo': precos[0] if len(precos) > 0 else None,
+                'preco_novo': precos[1] if len(precos) > 0 else None,
+                'rating': produto.css('span.poly-reviews__rating::text').get(),
+                'qtd_review': produto.css('span.andes-visually-hidden::text').get()
             }
