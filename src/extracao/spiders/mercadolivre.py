cac4ea9a77bed1bdefa7d95ddd5a8a60e5957dd7
Lucas de Jesus <lucas.scidata@gmail.com>
2024-11-18 15:48:38 -0300

    adicionando funcionalidade de webscraping em 10 paginas.

--- src/extracao/spiders/mercadolivre.py
+++ src/extracao/spiders/mercadolivre.py
@@ -11,6 +11,7 @@ class MercadolivreSpider(scrapy.Spider):
     def parse(self, response):
 
         produtos = response.css('div.ui-search-result__wrapper')
+        page_max = 1
 
         for produto in produtos:
 
@@ -22,5 +23,10 @@ class MercadolivreSpider(scrapy.Spider):
                 'preco_antigo': precos[0] if len(precos) > 0 else None,
                 'preco_novo': precos[1] if len(precos) > 0 else None,
                 'rating': produto.css('span.poly-reviews__rating::text').get(),
-                'qtd_review': produto.css('span.andes-visually-hidden::text').get()
+                'qtd_review': produto.css('span.poly-reviews__total::text').get()
             }
+
+        while page_max < 10:
+            next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
+            yield scrapy.Request(next_page, callback=self.parse)
+            page_max += 1
